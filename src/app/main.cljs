c532d11d59df4803780454a72cf027f5475520c1
Sean Hagstrom <sean@seanhagstrom.com>
2023-01-05 11:22:35 +0000

    add initial reactflow setup

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,4 +1,37 @@
-(ns app.main)
+(ns app.main
+  (:require
+   [reagent.core :as r]
+   ["react" :as react :refer [StrictMode]]
+   ["react-dom/client" :as react-dom]
+   ["reactflow" :as reactflow :refer [Background Controls ReactFlow]]))
+
+(defn make-client-root []
+  (let
+   [domNode (js/document.getElementById "root")]
+    (react-dom/createRoot domNode)))
+
+(defonce rf-background (r/adapt-react-class Background))
+(defonce rf-main (r/adapt-react-class ReactFlow))
+(defonce rf-controls (r/adapt-react-class Controls))
+(defonce strict-mode (r/adapt-react-class StrictMode))
+
+;; (defonce client-root (make-client-root))
+(defonce nodes
+  #js [#js {:id "1"
+            :position #js {:x 0 :y 0}}])
+
+(defonce props {:nodes nodes})
+(defonce client-root (make-client-root))
+
+(defn main-ui []
+  [strict-mode
+   [:div {:style {:height "100%"}}
+    [rf-main {:nodes nodes}
+     [rf-background]
+     [rf-controls]]]])
 
 (defn ^:export render []
-  (println "[main]: render"))
+  (println "[main]: render")
+  (let
+   [element (r/as-element [main-ui])]
+    (. client-root render (r/create-element StrictMode nil element))))
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './cljs-index.js'
 import './index.css'
+import 'reactflow/dist/style.css';
 
 if (module.hot) {
   module.hot.accept()
