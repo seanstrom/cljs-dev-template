b5ae07c22d105f04bb6044f2477aaeaf2a5d029e
Sean Hagstrom <sean@seanhagstrom.com>
2023-01-05 11:16:20 +0000

    setup shadow-cljs with Parcel's HMR

---
+++ src/cljs-index.js
@@ -0,0 +1,15 @@
+import app from '../cljs-dist/app.main.js'
+
+if (process.env.NODE_ENV !== "production") {
+  require("../cljs-dist/shadow.cljs.devtools.client.browser.js")
+}
+
+window.onload = () => {
+  app.render()
+}
+
+if (module.hot) {
+  module.hot.accept(() => {
+    app.render()
+  })
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import '../.parcel-cache/cljs-dist/main.js'
+import './cljs-index.js'
 import './index.css'
 
 if (module.hot) {
