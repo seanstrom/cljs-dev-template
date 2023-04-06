c8180a3a9a6ccc14f74ee80e5afc8165ebd07419
Sean Hagstrom <sean@seanhagstrom.com>
2023-04-06 15:01:32 +0100

    add some code to demo the glitch

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,4 +1,10 @@
 (ns app.main)
 
 (defn ^:export render []
+  (let [change-me "Hello"
+
+        node (js/document.getElementById "root")
+        text-node (js/document.createTextNode change-me)]
+    (.appendChild node text-node))
+
   (println "[main]: render"))
