cf2cb4fdfa677df28a47ff8653775c34560669f9
Sean Hagstrom <sean@seanhagstrom.com>
2023-12-08 12:45:09 +0000

    feature: add demo button for testing source maps

--- src/app/app.cljs
+++ src/app/app.cljs
@@ -7,6 +7,13 @@
         text-node (js/document.createTextNode change-me)]
     (.appendChild node text-node))
 
+  (let [button-element (js/document.createElement "button")]
+    (.appendChild js/document.body button-element)
+    (.addEventListener button-element "click"
+                       (fn [e]
+                         (throw (js/Error. "button clicked"))
+                         (println "[main]: button clicked"))))
+
   (println "[main]: render"))
 
 (defn ^:dev/after-load start []
