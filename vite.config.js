4768abba354aa9cdff35b986168e77cb0eb51cb4
Sean Hagstrom <sean@seanhagstrom.com>
2024-01-10 09:32:25 +0000

    feature: allow for clojurescript files to import javascript files through vite bundler

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import path from 'path'
 import { defineConfig } from "vite"
+import { fileURLToPath, URL } from 'url'
+
 
 export default defineConfig({
   server: {
     watch: {
       // ignored: ["**/cljs-dist/**/cljs-runtime/**"]
+    },
+  },
+  resolve: {
+    alias: {
+      "~js": path.resolve(__dirname, "src/"),
     },
   },
 })
