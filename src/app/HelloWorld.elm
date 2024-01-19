cf0fd28f24b48ca5ef6dd761947a18d57d917fe6
Sean Hagstrom <sean@seanhagstrom.com>
2024-01-19 12:22:58 +0000

    tidy: refactor HelloWorld module imports
    # 50-character subject line
    #
    # 72-character wrapped longer description.
    #
    # * Maybe try answering these questions: *
    # * What did I change? *
    # * Why did I change it? *
    # * How did I change it? *

--- src/app/HelloWorld.elm
+++ src/app/HelloWorld.elm
@@ -1,10 +1,9 @@
 module HelloWorld exposing (helloWorld)
 
 import Html exposing (Html, a, button, code, div, h1, p, text)
-import Html.Attributes exposing (href)
+import Html.Attributes exposing (class, href)
 import Html.Events exposing (onClick)
 import Msg exposing (Msg(..))
-import Html.Attributes exposing (class)
 
 
 helloWorld : Int -> Html Msg
