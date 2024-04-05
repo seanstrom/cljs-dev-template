9b1a76ea6d37c826f81ba178c3b9a1ea11b00c8e
Sean Hagstrom <sean@seanhagstrom.com>
2024-04-05 16:22:41 +0100

    tweak: add gleam import example

--- src/app/app.js
+++ src/app/app.js
@@ -3,12 +3,14 @@ import * as app from '#cljs/app/app.main.js'
 import { Elm } from "./Main.elm"
 import './app.css'
 
+import { add_one } from "./app.gleam"
+
 if (import.meta.hot) {
   import.meta.hot.accept()
 }
 
 window.onload = () => {
-  console.log('test')
+  console.log('test', add_one)
   const app = Elm.Main.init({ node: document.getElementById("root") })
   app.ports.outbox?.subscribe((msg) => {
     console.log("msg", msg)
