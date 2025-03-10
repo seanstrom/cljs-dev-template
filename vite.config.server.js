d42fc935d707daeccbedafcdf209f2b84801daff
Sean Hagstrom <sean@seanhagstrom.com>
2025-03-10 08:56:04 +0000

    wip: add initial dev-server configuration with vite and bun

---
+++ vite.config.server.js
@@ -0,0 +1,36 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import { VitePluginNode } from 'vite-plugin-node'
+
+export default defineConfig({
+  // ...vite configures
+  server: {
+    // vite server configs, for details see [vite doc](https://vitejs.dev/config/#server-host)
+    port: 3100,
+    hmr: true,
+  },
+  plugins: [
+    ...VitePluginNode({
+      // Nodejs native Request adapter
+      // currently this plugin support 'express', 'nest', 'koa' and 'fastify' out of box,
+      // you can also pass a function if you are using other frameworks, see Custom Adapter section
+      adapter: 'express',
+
+      // tell the plugin where is your project entry
+      appPath: './src/server/server.js',
+
+      // Optional, default: 'viteNodeApp'
+      // the name of named export of you app from the appPath file
+      exportName: 'app',
+
+      // Optional, default: false
+      // if you want to init your app on boot, set this to true
+      initAppOnBoot: true,
+    })
+  ],
+  resolve: {
+    alias: {
+      "#cljs": path.resolve(__dirname, "cljs-dist/"),
+    },
+  },
+})
